121159d61e2f7e7157fa6cc8bc4fa563c2d87fd6
Joao-vitor00 <joaovitorscarvalho2007@gmail.com>
2024-06-05 15:33:33 -0300

    preparando documento

---
+++ src/main/java/com/jvsc/fit/FitApplication.java
@@ -0,0 +1,13 @@
+package com.jvsc.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitApplication.class, args);
+	}
+
+}
